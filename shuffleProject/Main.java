6a5a7e1f69cbfda2acfa7259f8975a709dc84703
KoryunY <81442008+KoryunY@users.noreply.github.com>
2021-07-05 18:22:10 +0400

    Add files via upload

---
+++ shuffleProject/Main.java
@@ -0,0 +1,12 @@
+package shuffleProject;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> entryList = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        Reop.Menu(entryList,sc);
+    }
+
+}
---
+++ shuffleProject/Reop.java
@@ -0,0 +1,42 @@
+package shuffleProject;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Reop {
+    public static void Menu(List<String> entryList,Scanner sc){
+        boolean isActive=true;
+        int cmd;
+        while (isActive){
+            System.out.println("Enter command number");
+            System.out.println("1.Create new List");
+            System.out.println("2.shuffleProject created List");
+            System.out.println("3.Print List");
+            System.out.println("4.exit");
+            cmd= sc.nextInt();
+            switch (cmd){
+                case 1:
+                    entryList.clear();
+                    System.out.println("List Element Number");
+                    int count = sc.nextInt();
+                    for (int i = 1; i <= count; i++) {
+                        System.out.println("Enter " + i + " element Value");
+                        entryList.add(sc.next());
+                    }
+                    break;
+                case 2:
+                    Collections.shuffle(entryList);
+                    break;
+                case 3:
+                    System.out.println(entryList);
+                    break;
+                case 4:
+                    isActive=false;
+                    break;
+                default:
+                    System.out.println("Enter Valid Command");
+            }
+        }
+    }
+}
