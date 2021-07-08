091f2710c65c5fba0afaf74118e98b7cf8605400
KoryunY <81442008+KoryunY@users.noreply.github.com>
2021-07-08 12:15:03 +0400

    Add files via upload

--- typer/src/com/gmail/koryun/yeritsyan/typer/TopList.java
+++ typer/src/com/gmail/koryun/yeritsyan/typer/TopList.java
@@ -4,29 +4,34 @@ package com.gmail.koryun.yeritsyan.typer;
 import java.util.*;
 
 public class TopList {
-    static Map<Integer, String> topMap;
+    final static int TOP_COUNT = 3;
+    static NavigableMap<Integer, String> topMap;
 
     public TopList() {
         topMap = new TreeMap<>(Collections.reverseOrder());
     }
 
     public boolean checkScore(int score) {
-        if(topMap.size()==0)return true;
-        if (topMap.size() != 0) {
-            if (topMap.keySet().stream().findFirst().get() <= score) {
-                topMap.remove(score);
+        if (topMap.isEmpty()) return true;
+        if (topMap.size() == TOP_COUNT) {
+            int last=topMap.lastEntry().getKey();
+            if (last< score) {
+                topMap.remove(last);
                 return true;
             }
+            else return false;
         }
-        return false;
-    }
+        return true;
+}
 
     public void add(int score, String name) {
         topMap.put(score, name);
     }
-    public void clear(){
+
+    public void clear() {
         topMap.clear();
     }
+
     public void printList() {
         System.out.println("___________________________");
         for (int key :
